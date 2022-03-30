406604a290232059054a556c8d8d07fc6f7f31e6
Romain Rondet <mindsky@romains-macbook-pro.home>
2022-03-30 10:22:19 +0200

    Change a TODO state

--- backend/apps/todo/views.py
+++ backend/apps/todo/views.py
@@ -9,5 +9,5 @@ from .serializers import TodoSerializer
 class TodoViewSet(viewsets.ModelViewSet):
     """Categories model viewset."""
 
-    queryset = Todo.objects.all()
+    queryset = Todo.objects.all().order_by("state")
     serializer_class = TodoSerializer
--- frontend/src/views/TodoList.vue
+++ frontend/src/views/TodoList.vue
@@ -1,9 +1,20 @@
 <template>
   <div v-for="todo in todoList" :key="todo.id" class="container">
     <div class="card mb-3">
-      <div class="card-header">{{ todo.title }}</div>
       <div class="card-body">
-        <p class="card-text">{{ todo.description }}</p>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            @click="updateCurrentState(todo.id)"
+            type="checkbox"
+            value
+            id="flexCheckDefault"
+          />
+          <p v-if="todo.state == true" class="mb-0">
+            <strike>{{ todo.title }}</strike>
+          </p>
+          <p v-else class="mb-0">{{ todo.title }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -16,14 +27,33 @@ export default {
   name: 'TodoList',
   data() {
     return {
+      todoUrl: "http://127.0.0.1:8000/api-v1/todos/",
       todoList: [],
+      currentState: null,
     }
   },
 
   methods: {
     async getTodoList() {
-      const response = await axios.get("http://127.0.0.1:8000/api-v1/todos/");
+      const response = await axios.get(this.todoUrl);
       this.todoList = response.data;
+    },
+
+    async updateCurrentState(todo_id) {
+      const response = await axios.get(`${this.todoUrl}${todo_id}/`);
+      this.currentState = response.data["state"]
+      if (this.currentState == true) {
+        await axios.patch(`${this.todoUrl}${todo_id}/`, {
+          state: false
+        });
+        this.currentState = false;
+      } else {
+        await axios.patch(`${this.todoUrl}${todo_id}/`, {
+          state: true
+        })
+        this.currentState = true;
+      }
+      this.getTodoList();
     },
   },
 
